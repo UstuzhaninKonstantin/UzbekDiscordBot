e08ae49ed8be51b9d8de57b203770d26a1c37fbb
UstuzhaninKonstantin <themad.jr@gmail.com>
2022-05-27 11:20:25 +0300

    new comands

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import logging
 import random
 import json
 from discord.ext import commands
-from constants import TOKEN, PREFIX
+from constants import TOKEN, PREFIX, MESSAGE_ANSWER
 
 logger = logging.getLogger('discord')
 logger.setLevel(logging.WARNING)
@@ -15,7 +15,23 @@ intents = discord.Intents.default()
 intents.members = True
 
 
-bot = commands.Bot(command_PREFIX=PREFIX, intents=intents)
+bot = commands.Bot(command_prefix=PREFIX, intents=intents)
+
+
+@bot.command(name='answer_off')
+@commands.is_owner()
+async def answers_off(ctx):
+    global MESSAGE_ANSWER
+    MESSAGE_ANSWER = False
+    await ctx.send('Ответы на сообщения отключены.')
+
+
+@bot.command(name='answer_on')
+@commands.is_owner()
+async def answers_on(ctx):
+    global MESSAGE_ANSWER
+    MESSAGE_ANSWER = True
+    await ctx.send('Ответы на сообщения включены.')
 
 
 @bot.command(name='length')
@@ -29,25 +45,26 @@ async def number_of_words(ctx):
 async def on_message(message):
     if message.author == bot.user:
         return
-    msg = ''
-    for _ in range(3):
+    if MESSAGE_ANSWER and (message.content[0:len(PREFIX)] != PREFIX):
+        msg = ''
+        for _ in range(3):
+            with open("words.json", "r", encoding='utf-8') as f:
+                data = json.load(f)
+                words = data["words"]
+                msg += random.choice(words) + ' '
+
         with open("words.json", "r", encoding='utf-8') as f:
             data = json.load(f)
             words = data["words"]
-            msg += random.choice(words) + ' '
 
-    with open("words.json", "r", encoding='utf-8') as f:
-        data = json.load(f)
-        words = data["words"]
+        for word in message.content.split():
+            if word != '"' and (word not in words):
+                words.append(word)
 
-    for word in message.content.split():
-        if word != '"' and word[0:len(PREFIX)] != PREFIX and (word not in words):
-            words.append(word)
+        with open("words.json", "w", encoding='utf-8') as f:
+            json.dump({"words": words}, f, ensure_ascii=False)
 
-    with open("words.json", "w", encoding='utf-8') as f:
-        json.dump({"words": words}, f, ensure_ascii=False)
-
-    await message.channel.send(msg[0:-1])
+        await message.channel.send(msg[0:-1])
     await bot.process_commands(message)
 
 
